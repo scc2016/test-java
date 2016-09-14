d7bf9882d25b8e2da290e4c3370854f51e31181f
scc <scc@smtp.telek.com.cn>
2016-09-14 10:15:18 +0800

    增加一些ubuntu知识

--- test-java/src/HelloScc.java
+++ test-java/src/HelloScc.java
@@ -1,25 +1,7 @@
 import java.io.UnsupportedEncodingException;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 
 public class HelloScc {
 
 	public static void main(String[] args) throws UnsupportedEncodingException {
-		Map map = new TreeMap<String, String>();
-		map.get(null);
-
-		Iterator<Entry<String, String>> it = map.entrySet().iterator();
-		while (it.hasNext()) {
-			Entry<String, String> entry = it.next();
-			it.remove();
-		}
-		if (map.containsKey("sss")) {
-			System.out.println("sss");
-		} else {
-			map.get("sss");
-			System.out.println("111");
-		}
 	}
 }
